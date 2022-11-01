b426343443412328b17ae74a047d579be304e175
Viacheslav <public@religiosa.ru>
2022-11-01 21:51:32 +0100

    fix 4

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,4 +1,4 @@
-import { app } from "index";
+import { app } from "../index";
 import type { Update, SendMessageOptions } from "node-telegram-bot-api";
 import TelegramBot from "node-telegram-bot-api";
 import { botRoutes } from "./BotRoutes";
