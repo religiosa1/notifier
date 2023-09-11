6b76691d8bebe6b7dbbc87cf017589766b8ff7df
Viacheslav <public@religiosa.ru>
2023-09-11 23:42:50 +0200

    SettingsService subscribe

--- packages/server/src/services/SettingsService.ts
+++ packages/server/src/services/SettingsService.ts
@@ -10,11 +10,11 @@ export class SettingsService {
 
 	private config: ServerConfig | undefined;
 	private storagePath: string = __dirname;
-	private emitter = new Emitter<{ change(config: ServerConfig): unknown }>();
+	private emitter = new Emitter<{ change(config: ServerConfig, oldConfig?: ServerConfig): unknown }>();
 
-	async loadConfig(storagePath: string): Promise<ServerConfig> {
-		const currentConfigName = join(storagePath, this.currentConfigName);
-		const initialConfigName = join(storagePath, this.initialConfigName);
+	async loadConfig(): Promise<ServerConfig> {
+		const currentConfigName = join(this.storagePath, this.currentConfigName);
+		const initialConfigName = join(this.storagePath, this.initialConfigName);
 
 		const fileToGet = await isReadable(currentConfigName) ? currentConfigName : initialConfigName;
 		const dataString = await readFile(fileToGet, "utf8");
@@ -33,11 +33,21 @@ export class SettingsService {
 			JSON.stringify(config, undefined, 4),
 			"utf8"
 		);
+		const oldConfig = this.config;
 		this.config = config;
+		this.emitter.emit("change", config, oldConfig);
 	}
 
-	subscribe(cb: (config: ServerConfig) => unknown): () => void {
-		return this.emitter.on("change", cb);
+	subscribe(
+		cb: (config: ServerConfig, oldConfig?: ServerConfig) => unknown,
+		fields?: Array<keyof ServerConfig>
+	): () => void {
+		return this.emitter.on("change", (config, oldConfig) => {
+			const shouldCall = fields?.some((field) => config[field] !== oldConfig?.[field]) ?? true;
+			if (shouldCall) {
+				cb(config, oldConfig);
+			}
+		});
 	}
 	unsubscribeAll(): void {
 		this.emitter.clear();
