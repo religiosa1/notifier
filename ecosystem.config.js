7a90f87b4bb03ad16ad7c84878f62376eaabf0ab
Viacheslav <public@religiosa.ru>
2023-12-27 09:48:46 +0100

    production env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,30 @@
 module.exports = {
   apps : [
 		{
-			name   : "notifier",
-			script : "./packages/server/dist/index.js",
+			name: "notifier",
+			script: "./packages/server/dist/index.js",
 			env_production: {
 				NODE_ENV: "production"
 			},
 			env_development: {
 				NODE_ENV: "development"
-			}
+			},
 		},
 		{
-			name   : "notifier-admin",
-			script : "./packages/admin/build/index.js"
+			name: "notifier-admin",
+			script: "./packages/admin/build/index.js",
+			env: {
+				API_URL: "http://127.0.0.1:8085/",
+			},
+			env_production: {
+				NODE_ENV: "production",
+				PROTOCOL_HEADER: "X-Forwarded-Proto",
+				HOST_HEADER: "X-Forwarded-Host",
+			},
+			env_development: {
+				NODE_ENV: "development",
+				ORIGIN: "http://localhost:5173/",
+			},
 		},
 	]
 }
